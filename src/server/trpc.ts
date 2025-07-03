e57b5f1eb82c0a8e241fa157914a05d6b1b317a1
Dallas S <andrewdallassherman@gmail.com>
2025-07-03 12:37:46 +0900

    refactor(tRPC): trpc now has UserPayload for better integration.

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -2,7 +2,7 @@ import { initTRPC, TRPCError } from "@trpc/server";
 import { type FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import superjson from "superjson";
 import { parse } from "cookie";
-import { tokenService } from "@/services/token";
+import { tokenService, UserPayload } from "@/services/token";
 
 // init tRPC
 const t = initTRPC.context<typeof createTRPCContext>().create({
@@ -11,23 +11,60 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 
 // Context creation function
 export const createTRPCContext = async (opts: FetchCreateContextFnOptions) => {
-  const { req } = opts;
+  const { req, resHeaders } = opts;
   const cookies = parse(req.headers.get("cookie") ?? "");
   const token = cookies.accessToken;
+  const ip =
+    req.headers.get("x-forwarded-for") ||
+    req.headers.get("x-real-ip") ||
+    "unknown";
+
+  const context: {
+    req: typeof req;
+    resHeaders: typeof resHeaders;
+    user: UserPayload | null;
+    ip: string;
+    cookies: Record<string, string | undefined>;
+  } = {
+    req,
+    resHeaders,
+    user: null,
+    ip,
+    cookies,
+  };
 
   if (!token) {
-    return { user: null };
+    return context;
   }
 
   try {
     const userPayload = await tokenService.verifyToken(token);
-    return { user: userPayload };
+    context.user = userPayload;
+    return context;
   } catch {
-    return { user: null };
+    return context;
   }
 };
 
-// Middleware
+const isTempAuthenticated = t.middleware(async ({ ctx, next }) => {
+  const tempToken = ctx.req.headers.get("authorization")?.split(" ")[1];
+
+  if (!tempToken) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "No token provided" });
+  }
+
+  try {
+    const userPayload = await tokenService.verifyTempToken(tempToken);
+    return next({
+      ctx: {
+        ...ctx,
+        user: userPayload,
+      },
+    });
+  } catch {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid token" });
+  }
+});
 const isAuthenticated = t.middleware(({ ctx, next }) => {
   if (!ctx.user) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
@@ -41,5 +78,6 @@ const isAuthenticated = t.middleware(({ ctx, next }) => {
 });
 
 export const router = t.router;
-export const publicProcedure = t.procedure; // Public route
-export const protectedProcedure = t.procedure.use(isAuthenticated); // Private route
+export const publicRoute = t.procedure; // Public route
+export const protectedRoute = t.procedure.use(isAuthenticated);
+export const tempAuthRoute = t.procedure.use(isTempAuthenticated);
