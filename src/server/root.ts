c0837a559828b888487b4cd8f3c6756ae7533869
Dallas S <andrewdallassherman@gmail.com>
2025-08-03 13:48:02 +0900

    feat: adding todoRouter to appRouter

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,10 +1,12 @@
 import { router } from "@/server/trpc";
 import { userRouter } from "@/server/routers/user";
 import { noteRouter } from "@/server/routers/note";
+import { todoRouter } from "@/server/routers/todo";
 
 export const appRouter = router({
   user: userRouter,
   note: noteRouter,
+  todo: todoRouter,
 });
 
 // Export type definition of API
